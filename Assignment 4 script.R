cc0070aed17c8e1881ce67be81175c31815c75b9
SehajRaina <14skr@queensu.ca>
2019-01-31 20:18:12 -0500

    Final commit

--- Assignment 4 script.R
+++ Assignment 4 script.R
@@ -1,49 +1,45 @@
+###Assignment 4 
+###Sehaj Raina
+
+
+#1. Load packages 
 
 library(BiocManager)
 library(sangerseqR)
-library(seqinr)
+library(seqinr) #for exporting data into Fasta file 
 
 
-SeqStats<-read.csv("Data/BarcodePlateStats.csv") #quality control of sequence statistics
+#2. Quality control of sequence statistics
 
-GoodQuality<-subset(SeqStats,Ok=="TRUE")
+SeqStats<-read.csv("Data/BarcodePlateStats.csv") #load data into object 
+GoodQuality<-subset(SeqStats,Ok=="TRUE") #subset rows that are OK aka. TRUE for the quality check 
 
-# get full names including folder path
-filesName<-list.files("Data", pattern=".ab1", full.names = TRUE)
+filesName<-list.files("Data", pattern=".ab1", full.names = TRUE) #get full names including folder path of all sanger sequencing files
+filesName2<-filesName[basename(filesName) %in% GoodQuality$Chromatogram] #Only keep the sanger sequencing files whos basename (file names) match in GoodQuality$Chromatogram column
 
-# then keep only the basename (file names) matching dataframe column
-filesName2 <- filesName[basename(filesName) %in% GoodQuality$Chromatogram]
-basename(filesName2)
-# then read the data
+
+#3. Read, extract and call the data using lapply function to apply function to all elements of a list 
+
 ITS<-lapply(filesName2, read.abif) #read
+ITSseq<-lapply(ITS, sangerseq) #extract
+SeqX<-lapply(ITSseq, makeBaseCalls) #call
 
-ITSseq <- lapply(ITS, sangerseq) #extract
 
-SeqX <- lapply(ITSseq, makeBaseCalls) #call
-
+#4. Extract Primary Sequence and place in a list 
 
 SeqX1<-list() #blank list
 SeqX2<-list() #blank list
 
 
 for(i in 1:length(SeqX)){
-    SeqX1[i]<-SeqX[[i]]@primarySeq
-    SeqX2[i]<-gsub(".*seq","",SeqX1[[i]])  #only get the primary sequence and put it in a list
+    SeqX1[i]<-SeqX[[i]]@primarySeq #only get the primary sequence and put it in SeqX1 list
+    SeqX2[i]<-gsub(".*seq","",SeqX1[[i]]) #use regular expression on SeqX1 to remove everything before the sequence itself and put it into new SeqX2 list
 }
 
 
+#5. Export primary sequence data to a FASTA file 
 
-plzWORK <- paste(" ", data2$new, data1$V1, sep="\n")
-
-cat(plzWORK)
-
-write.table(plzWORK, file="omg", append = FALSE, sep = " ", dec = ".",
-            row.names = FALSE, col.names = FALSE)
-
-df <- read.table("omg", header = FALSE)
-
-
-
-getwd()
-library(seqinr)
-write.fasta(sequences=SeqX2,names=basename(filesName2),nbchar = 80, as.string = TRUE, file.out="DNA")
+DNAlength<-paste("length=",nchar(SeqX2),";", sep="") #create object with length of each sequence, FASTA style 
+gene<-paste(basename(filesName2), DNAlength, "type=DNA", sep=" ") #create object stringing each file name to it's respective sequence length from DNAlength, FASTA style
+  
+write.fasta(sequences=SeqX2,names=gene,nbchar = 80, as.string = TRUE, file.out="DNA") #assign names from gene object to each respective sequence and output into FASTA format file
